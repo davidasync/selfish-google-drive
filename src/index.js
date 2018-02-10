ffee541a1b5b581bb7e4066e4b6a879b9f526eb4
davidasync <davidasync@gmail.com>
2018-02-10 23:06:21 +0700

    Add get request token

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 const _ = require('lodash');
+const getAccessToken = require('./lib/getAccessToken');
+
+const library = {
+  getAccessToken,
+};
 
 /**
  * Load all possible wrapper google drive api
@@ -9,5 +14,13 @@ const _ = require('lodash');
  * @return {Object.<function>}
  */
 module.exports = (credential) => {
-  return {};
+  const clientID = _.get(credential, 'clientID');
+  const clientSecret = _.get(credential, 'clientSecret');
+  const refreshToken = _.get(credential, 'refreshToken');
+
+  if (!clientID || !clientSecret || !refreshToken) {
+    throw new Error('Should provide clientId, clientSecret, and refreshToken');
+  }
+
+  return _.mapValues(library, func => _.partial(func, credential));
 };
--- src/lib/getAccessToken.js
+++ src/lib/getAccessToken.js
@@ -6,6 +6,40 @@ const superagent = require('superagent');
 
 const request = bluebird.promisifyAll(superagent);
 
-module.exports = (credential) => {
+const constant = require('../../utils/constants');
 
+/**
+ * Get access token
+ * Response body should be like this
+ * {
+ *   access_token: 'somestring'
+ *   token_type: 'Bearer',
+ *   expires_in: '3600'
+ * }
+ * @param {Object} credential
+ * @param {Object} credential.clientID
+ * @param {Object} credential.clientSecret
+ * @param {Object} credential.refreshToken
+ * @return {Object}
+ */
+module.exports = (credential) => {
+  const data = {
+    grant_type: 'refresh_token',
+    client_id: _.get(credential, 'clientID'),
+    client_secret: _.get(credential, 'clientSecret'),
+    refresh_token: _.get(credential, 'refreshToken'),
+  };
+
+  return request
+    .post(constant.refreshTokenUrl)
+    .send(data)
+    .set('content-type', 'application/x-www-form-urlencoded')
+    .endAsync()
+    .then((response) => {
+      if (_.get(response, 'statusCode') !== 200) {
+        throw new Error(_.get(response, 'body'));
+      }
+
+      return _.get(response, 'body');
+    });
 };
